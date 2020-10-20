eb8e91e58c1f8758acc9ded350090ce4efefc354
Arthur Sultanov <as@boolqa.com>
2020-10-20 21:17:32 +0300

    Initial commit

---
+++ Task1810/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1810
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
